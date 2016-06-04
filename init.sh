d2126b87a9d8f4c169c79cf0dcbdad5d14b8c31c
Peter Watkins <pdwatkins@me.com>
2016-06-04 10:00:30 -0700

    fiddling

--- init.sh
+++ init.sh
@@ -23,11 +23,11 @@ if [ -z "$DATABASE_NAME" ]; then
 fi
 
 if [ -z "$DB_TYPE" ]; then
-	export DB_TYPE=$MYSQL_VERSION
+	export DB_TYPE="MYSQL"
 fi
 
 if [ -z "$DB_MANAGER" ]; then
-	export DB_MANAGER="hello"
+	export DB_MANAGER=""
 fi
 
 /usr/local/bin/envtemplate.py -i /usr/local/src/config_override.php.pyt -o /var/www/html/config_override.php
